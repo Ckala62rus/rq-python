8f91d95f6b438a7723f62becfbdce444df7fdb61
Ckala62rus <agr.akyla@mail.ru>
2025-03-23 13:21:25 +0300

    ( feature ) added rq_config

---
+++ src/rq_conf.py
@@ -0,0 +1,8 @@
+REDIS_HOST = "rq_redis"
+# REDIS_PORT = port
+# REDIS_PASSWORD = "password"
+# REDIS_SSL = True
+# REDIS_SSL_CA_CERTS = None
+REDIS_DB = 0
+# REDIS_SSL_CERT_REQS = None
+# REDIS_SSL_CA_DATA = "-----BEGIN CERTIFICATE-----\n****"
